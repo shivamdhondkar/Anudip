bfb18a4197081db29ad1f61888f6cba0e2868624
Shivam Dhondkar <161524958+shivamdhondkar@users.noreply.github.com>
2025-07-02 23:48:59 +0530

    Add files via upload

---
+++ ArrayPro.java
@@ -0,0 +1,12 @@
+
+public class ArrayPro {
+    public static void main(String[] args) {
+        int  arr [] =  new int[5];
+        arr[0] = 5;
+        for (int i = 0;i<=4;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
